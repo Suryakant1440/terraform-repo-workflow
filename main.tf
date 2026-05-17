128c9574a02f90b0a44c849186ba3df5cf3eb8c1
Suryakant1440 <suryakant.maurya85@gmail.com>
2026-05-17 16:28:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,19 @@ resource "azrerm_resource_group" "name" {
 name = "krishna-rg"
 location = "East US"
 }
-provider "azurerm" {
-features {
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
 }
-subscription_id = "5d297d85-ddad-4ac7-8baa-2ce25e3f0ce5"
+
+provider "azurerm" {
+  features {}
+  subscription_id = "5d297d85-ddad-4ac7-8baa-2ce25e3f0ce5"
 }
