a74f2c2c4f9a349e19f32275a6f1fa2d96f38b0a
Suryakant1440 <suryakant.maurya85@gmail.com>
2026-05-17 16:33:39 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "5d297d85-ddad-4ac7-8baa-2ce25e3f0ce5"
+}
