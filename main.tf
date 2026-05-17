eac19b3974bdaae71c3cdc10147f326b283798f0
Suryakant1440 <suryakant.maurya85@gmail.com>
2026-05-17 16:37:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "name" {
-name = "krishna-rg"
-location = "East US"
+resource "azurerm_resource_group" "rg" {
+  name = "krishna-rg"
+  location = "East US"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "my-rg"
+  location = "Central India"
 }
